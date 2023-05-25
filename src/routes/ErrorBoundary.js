5a067f542aa307171081614e17b8a0f5f825885c
TheSonOfBat34 <TheSonOfBat@gmail.com>
2023-05-25 13:35:44 +1000

    Update ErrorBoundary.js

--- src/routes/ErrorBoundary.js
+++ src/routes/ErrorBoundary.js
@@ -10,10 +10,10 @@ export default function ErrorBoundary(){
             <h1 style={{fontSize: "6rem"}}>Whoops...</h1>
             <p style={{fontSize: "1.5rem", maxWidth: "700px"}}>Looks like you went outside of the scope of the project. Please use one of these links to return back to your desired page.</p>
             <div className="nav--secondaryIcons" style={{flexDirection: "row", width: "200px", height: "min-content", marginTop: "30px"}}>
-                <a href="/"><img className={`nav--icon ${window.location.pathname==="/"?"nav--iconHighlighted":""}`} src={navHome}></img></a>
-                <a href="/movie"><img className={`nav--icon ${window.location.pathname==="/movie"?"nav--iconHighlighted":""}`} src={navMovie}></img></a>
-                <a href="/tv"><img className={`nav--icon ${window.location.pathname==="/tv"?"nav--iconHighlighted":""}`} src={navTv}></img></a>
-                <a href="/bookmark"><img className={`nav--icon ${window.location.pathname==="/bookmark"?"nav--iconHighlighted":""}`} src={navBookmark}></img></a>
+                <a href="entertainment-app/"><img className={`nav--icon ${window.location.pathname==="/"?"nav--iconHighlighted":""}`} src={navHome}></img></a>
+                <a href="entertainment-app/movie"><img className={`nav--icon ${window.location.pathname==="/movie"?"nav--iconHighlighted":""}`} src={navMovie}></img></a>
+                <a href="entertainment-app/tv"><img className={`nav--icon ${window.location.pathname==="/tv"?"nav--iconHighlighted":""}`} src={navTv}></img></a>
+                <a href="entertainment-app/bookmark"><img className={`nav--icon ${window.location.pathname==="/bookmark"?"nav--iconHighlighted":""}`} src={navBookmark}></img></a>
             </div>
         </div>
     )
