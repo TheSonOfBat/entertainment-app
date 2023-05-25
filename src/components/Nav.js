5f06006610ffedd5caf61b7ef98ecb6d7bb8d06a
TheSonOfBat34 <TheSonOfBat@gmail.com>
2023-05-25 13:45:10 +1000

    Summary

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -10,10 +10,10 @@ export default function Nav(props){
         <nav>
             <a href="#"><img className="nav--logo" src={navLogo}></img></a>
             <div className="nav--secondaryIcons">
-                <a href="entertainment-app/"><img className={`nav--icon ${window.location.pathname==="/"?"nav--iconHighlighted":""}`} src={navHome}></img></a>
-                <a href="entertainment-app/movie"><img className={`nav--icon ${window.location.pathname==="/movie"?"nav--iconHighlighted":""}`} src={navMovie}></img></a>
-                <a href="entertainment-app/tv"><img className={`nav--icon ${window.location.pathname==="/tv"?"nav--iconHighlighted":""}`} src={navTv}></img></a>
-                <a href="entertainment-app/bookmark"><img className={`nav--icon ${window.location.pathname==="/bookmark"?"nav--iconHighlighted":""}`} src={navBookmark}></img></a>
+                <a href="/entertainment-app/"><img className={`nav--icon ${window.location.pathname==="/"?"nav--iconHighlighted":""}`} src={navHome}></img></a>
+                <a href="/entertainment-app/movie"><img className={`nav--icon ${window.location.pathname==="/movie"?"nav--iconHighlighted":""}`} src={navMovie}></img></a>
+                <a href="/entertainment-app/tv"><img className={`nav--icon ${window.location.pathname==="/tv"?"nav--iconHighlighted":""}`} src={navTv}></img></a>
+                <a href="/entertainment-app/bookmark"><img className={`nav--icon ${window.location.pathname==="/bookmark"?"nav--iconHighlighted":""}`} src={navBookmark}></img></a>
             </div>
             <img src={navAvatar} id="nav--avatar" onClick={props.alertFunction} style={{cursor: "pointer"}}></img>
         </nav>
